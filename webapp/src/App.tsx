783e704af70dceada0333a6e0eeda23aef72e545
Constantin Catalina <catalina.constantin@student.upt.ro>
2025-12-05 11:29:48 +0200

    remove admin and hr routes

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,8 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import HrDashboard from './pages/HrDashboard';
-import AdminDashboard from './pages/AdminDashboard';
 import './App.css';
 
 function App() {
@@ -12,8 +10,6 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/hr/dashboard" element={<HrDashboard />} />
-      <Route path="/admin/dashboard" element={<AdminDashboard />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
