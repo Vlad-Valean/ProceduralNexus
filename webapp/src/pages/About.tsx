ea7832d0ff681ebd81602c49ba7cf4d91fa56c58
Constantin Catalina <catalina.constantin@student.upt.ro>
2025-12-06 21:48:35 +0200

    add frontend routes and default pages

---
+++ webapp/src/pages/About.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const About: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <h1>About</h1>
+      </main>
+    </>
+  );
+};
+
+export default About;
---
+++ webapp/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <h1>Home</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
