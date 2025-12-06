b5e93cf249970d06effdbd0ba08fa02117df986b
Constantin Catalina <catalina.constantin@student.upt.ro>
2025-12-06 13:15:42 +0200

    add user table component

---
+++ webapp/src/components/OrganizationStats.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Paper } from "@mui/material";
+
+const OrganizationStats: React.FC = () => {
+  return (
+    <Paper style={{ padding: 24, borderRadius: 20, background: "#fff", minHeight: 630, boxShadow: "0 2px 16px #bfcbe6", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      
+    </Paper>
+  );
+};
+
+export default OrganizationStats;
