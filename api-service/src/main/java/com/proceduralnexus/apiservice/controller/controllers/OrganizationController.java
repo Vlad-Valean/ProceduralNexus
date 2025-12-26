d5711e9b8954681fb3b2dd2df6d5d50998b3ed37
Constantin Catalina <catalina.constantin@student.upt.ro>
2025-12-26 02:59:53 +0200

    connect backend to frontend

--- api-service/src/main/java/com/proceduralnexus/apiservice/controller/controllers/OrganizationController.java
+++ api-service/src/main/java/com/proceduralnexus/apiservice/controller/controllers/OrganizationController.java
@@ -1,20 +1,29 @@
 package com.proceduralnexus.apiservice.controller.controllers;
 
+import java.util.List;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.proceduralnexus.apiservice.business.interfaces.IOrganizationService;
 import com.proceduralnexus.apiservice.business.services.ProfileService;
 import com.proceduralnexus.apiservice.controller.dtos.OrganizationCreateDto;
 import com.proceduralnexus.apiservice.controller.dtos.OrganizationResponseDto;
 import com.proceduralnexus.apiservice.controller.dtos.OrganizationUpdateDto;
 import com.proceduralnexus.apiservice.data.entities.Profile;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
